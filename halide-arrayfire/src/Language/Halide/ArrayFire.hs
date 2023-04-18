c9c354e74dbb580a244056af67149a6176ef2559
twesterhout <14264576+twesterhout@users.noreply.github.com>
2023-04-18 09:51:35 +0200

    Passing CPU buffers between Halide and ArrayFire

--- halide-arrayfire/src/Language/Halide/ArrayFire.hs
+++ halide-arrayfire/src/Language/Halide/ArrayFire.hs
@@ -1,4 +1,32 @@
-module Language.Halide.ArrayFire (someFunc) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Language.Halide.ArrayFire () where
+
+import ArrayFire (AFType, Array)
+import ArrayFire qualified as AF
+import Data.Proxy
+import GHC.TypeLits
+import Language.Halide
+
+forceNumDims :: AFType a => Array a -> Int -> [Int]
+forceNumDims arr n
+  | AF.getNumDims arr <= n = take n shape
+  | otherwise =
+      error $
+        "cannot treat a "
+          <> show (AF.getNumDims arr)
+          <> "-dimensional array as a "
+          <> show n
+          <> "-dimensional buffer"
+  where
+    shape = let (d0, d1, d2, d3) = AF.getDims arr in [d0, d1, d2, d3]
+
+instance (IsHalideType a, AFType a, KnownNat n, n <= 4) => IsHalideBuffer (Array a) n a where
+  withHalideBufferImpl arr action = case AF.getBackend arr of
+    AF.CPU -> AF.withDevicePtr arr $ \ptr -> bufferFromPtrShape ptr shape action
+    AF.CUDA -> undefined
+    AF.OpenCL -> undefined
+    AF.Default -> error "do not know how to handle 'Default' backend"
+    where
+      shape = forceNumDims arr . fromIntegral $ natVal (Proxy @n)
--- halide-arrayfire/test/Spec.hs
+++ halide-arrayfire/test/Spec.hs
@@ -1,4 +1,16 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main (main) where
 
+import ArrayFire qualified as AF
+import Language.Halide
+import Language.Halide.ArrayFire
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main =
+  hspec $
+    describe "IsHalideBuffer" $ do
+      it "supports CPU arrays" $ do
+        let arr = 1 + AF.moddims (AF.range @Double [6] (-1)) [2, 3]
+        withHalideBuffer @2 @Double arr peekToList `shouldReturn` [[1, 3, 5], [2, 4, 6]]
